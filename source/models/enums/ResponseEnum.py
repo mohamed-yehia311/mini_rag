5ade83aabd310457c2eed5eebbaee06f67415131
mohamed-yehia311 <mmoa311311@gmail.com>
2024-11-29 12:49:34 +0200

    update ResponseEnums

--- source/models/enums/ResponseEnum.py
+++ source/models/enums/ResponseEnum.py
@@ -7,3 +7,5 @@ class ResponseSignal(Enum):
     FILE_SIZE_EXCEEDED = "file_size_exceeded"
     FILE_UPLOAD_SUCCESS = "file_upload_success"
     FILE_UPLOAD_FAILED = "file_upload_failed"
+    FILE_PROCESSING_FAILD = "file_processing_failed"
+    FILE_PROCESSING_SUCESS = "file_processing_success"

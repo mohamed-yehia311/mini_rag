b57c1daae0612cb3b3cfcc4229cca75c6ca9ca12
mohamed-yehia311 <mmoa311311@gmail.com>
2024-12-13 21:42:59 +0200

    update main

--- source/main.py
+++ source/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from routes import base, data
-
+from motor.motor_asyncio import AsyncIOMotorClient
+from helpers.config import get_setting
 
 app = FastAPI()
+
+@app.on_event("startup")
+async def startup_db_client():
+    settings = get_setting()
+    app.mongo_conn =  AsyncIOMotorClient(settings.MONGODB_URL)
+    app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
+    
+@app.on_event("shutdown")
+async def shutdown_db_client():
+    app.mongo_conn.close()
+
+
 app.include_router(base.baseRouter)
 app.include_router(data.dataRouter)

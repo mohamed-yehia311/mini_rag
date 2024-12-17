3ea1e5e937c2f4a1ab53fe8af743621f572a6261
mohamed-yehia311 <mmoa311311@gmail.com>
2024-12-18 01:10:04 +0200

    update controllers to habdel if file id is none

--- source/controllers/ProcessController.py
+++ source/controllers/ProcessController.py
@@ -7,31 +7,45 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from models import ProcessingEnum
 
 class ProcessController(BaseController):
+    
+    
     def __init__(self, project_id:str):
         super().__init__()
         self.project_id = project_id
         self.project_path = ProjectController().get_project_path(project_id=project_id)
     
-    def get_file_extention(self, file_id:str):
+    def get_file_extension(self, file_id:str):
         return os.path.splitext(file_id)[-1]
     
-    def get_file_loader(self, file_id:str):
-        file_extention = self.get_file_extention(file_id)
-        file_path = os.path.join(self.project_path, file_id)
+    def get_file_loader(self, file_id: str):
+    
+        file_ext = self.get_file_extension(file_id=file_id)
+        file_path = os.path.join(
+            self.project_path,
+            file_id
+        )
 
-        if file_extention == ProcessingEnum.TXT.value:
-            return TextLoader(file_path, encoding= "utf-8")
-        
-        if file_extention == ProcessingEnum.PDF.value:
+        if not os.path.exists(file_path):
+            return None
+
+        if file_ext == ProcessingEnum.TXT.value:
+            return TextLoader(file_path, encoding="utf-8")
+
+        if file_ext == ProcessingEnum.PDF.value:
             return PyMuPDFLoader(file_path)
         
         return None
 
     def get_file_content(self, file_id:str):
+        
         loader = self.get_file_loader(file_id=file_id)
-        return loader.load()
+        if loader:
+            return loader.load()
+        
+        return None
     
     def process_content(self, file_content: list, chunk_size:int = 100, overlap_size:int = 20):
+        
         text_splitter = RecursiveCharacterTextSplitter(
             chunk_size= chunk_size,
             chunk_overlap = overlap_size,

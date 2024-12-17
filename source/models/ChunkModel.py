d88b864e3b83cdb89042bf767872f71d54ca2984
mohamed-yehia311 <mmoa311311@gmail.com>
2024-12-18 01:13:23 +0200

    update chunck and project models for indexing

--- source/models/ChunkModel.py
+++ source/models/ChunkModel.py
@@ -1,5 +1,5 @@
 from .BaseDataModel import BaseDataModel
-from models.db_schemes import DataChunck
+from models.db_schemes import DataChunk
 from .enums.DBEnum import DBEnum
 from bson.objectid import ObjectId
 from pymongo import InsertOne
@@ -9,10 +9,29 @@ class ChunkModel(BaseDataModel):
     def __init__(self, db_client: object):
         super().__init__(db_client=db_client)
         self.collection = self.db_client[DBEnum.COLECTION_CHUNK_NAME.value]
+        
+    @classmethod
+    async def create_instance(cls, db_client: object):
+        instance = cls(db_client)
+        await instance.init_collection()
+        return instance
 
-    async def create_chunk(self, chunk: DataChunck):
+    async def init_collection(self):
+        all_collections = await self.db_client.list_collection_names()
+        if DBEnum.COLLECTION_ASSET_NAME.value not in all_collections:
+            self.collection = self.db_client[DBEnum.COLLECTION_ASSET_NAME.value]
+            indexes = DataChunk.get_indexes()
+            for index in indexes:
+                await self.collection.create_index(
+                    index["key"],
+                    name=index["name"],
+                    unique=index["unique"]
+                )
+
+
+    async def create_chunk(self, chunk: DataChunk):
         result = await self.collection.insert_one(chunk.dict(by_alias=True, exclude_unset=True))
-        chunk._id = result.inserted_id
+        chunk.id = result.inserted_id
         return chunk
 
     async def get_chunk(self, chunk_id: str):
@@ -23,7 +42,7 @@ class ChunkModel(BaseDataModel):
         if result is None:
             return None
         
-        return DataChunck(**result)
+        return DataChunk(**result)
 
     async def insert_many_chunks(self, chunks: list, batch_size: int=100):
 
--- source/models/ProjectModel.py
+++ source/models/ProjectModel.py
@@ -1,22 +1,46 @@
 from .BaseDataModel import BaseDataModel
 from models.db_schemes import Project
 from .enums.DBEnum import DBEnum
-
+from bson import ObjectId
 class ProjectModel(BaseDataModel):
     def __init__(self, db_client: object):
         super().__init__(db_client)
         self.collection = self.db_client[DBEnum.COLLECTION_PROJECT_NAME.value]
+        
+        
+    @classmethod
+    async def create_instance(cls, db_client: object):
+        instance = cls(db_client)
+        await instance.init_collection()
+        return instance
 
+    async def init_collection(self):
+        all_collections = await self.db_client.list_collection_names()
+        if DBEnum.COLLECTION_ASSET_NAME.value not in all_collections:
+            self.collection = self.db_client[DBEnum.COLLECTION_ASSET_NAME.value]
+            indexes = Project.get_indexes()
+            for index in indexes:
+                await self.collection.create_index(
+                    index["key"],
+                    name=index["name"],
+                    unique=index["unique"]
+                )
+    
+    
+    
     async def create_project(self, project:Project):
-        result =await self.collection.insert_one(project.dict())
-        project._id = result.inserted_id
+        
+        result =await self.collection.insert_one(project.dict(by_alias=True, exclude_unset=True))
+        project.id = result.inserted_id
 
         return project
     
     async def get_project_or_create (self, project_id:str):
+        
         record = await self.collection.find_one({
-            project_id : project_id
+            "project_id" : project_id,
         })
+        
         if record is None:
             project = Project(project_id = project_id)
             project = await self.create_project(project=project)

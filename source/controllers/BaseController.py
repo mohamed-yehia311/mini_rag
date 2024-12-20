5499e42f59099606b164e8e1342979b8ac32ab2e
mohamed-yehia311 <mmoa311311@gmail.com>
2024-12-20 22:26:20 +0200

    add: database dir attribute

--- source/controllers/BaseController.py
+++ source/controllers/BaseController.py
@@ -7,5 +7,8 @@ class BaseController:
         self.settings = get_setting()
         self.base_dir = os.path.dirname(os.path.dirname(__file__))
         self.files_dir = os.path.join(self.base_dir, "assests/files")
+        
+        self.database_dir = os.path.join(self.base_dir,"assets/Vector_database")
+
     def generate_random_string(self, length: int=12):
         return ''.join(random.choices(string.ascii_lowercase + string.digits, k=length))

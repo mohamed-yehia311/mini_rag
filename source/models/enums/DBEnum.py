40827c3208684ed9eca6b4407499538ceb75144f
mohamed-yehia311 <mmoa311311@gmail.com>
2025-06-17 04:47:52 +0300

    fix: correct typo in COLLECTION_CHUNK_NAME in DBEnum.py

--- source/models/enums/DBEnum.py
+++ source/models/enums/DBEnum.py
@@ -3,5 +3,5 @@ from enum import Enum
 class DBEnum(Enum):
 
     COLLECTION_PROJECT_NAME = "Projects"
-    COLECTION_CHUNK_NAME = "Chunks"
+    COLLECTION_CHUNK_NAME = "Chunks"
     COLLECTION_ASSET_NAME = "assets"

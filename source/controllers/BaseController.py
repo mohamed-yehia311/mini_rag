2574887a51aae45e362d1834f380d39b05b21dd2
mohamed-yehia311 <mmoa311311@gmail.com>
2025-06-17 04:43:18 +0300

    fix: correct method name and file path in BaseController

--- source/controllers/BaseController.py
+++ source/controllers/BaseController.py
@@ -1,14 +1,25 @@
-from helpers.config import get_setting, Settings
+from helpers.config import get_settings, Settings
 import os
 import random
 import string
 class BaseController:
     def __init__(self):
-        self.settings = get_setting()
+        self.settings = get_settings()
         self.base_dir = os.path.dirname(os.path.dirname(__file__))
-        self.files_dir = os.path.join(self.base_dir, "assests/files")
+        self.files_dir = os.path.join(self.base_dir, "assets/files")
         
         self.database_dir = os.path.join(self.base_dir,"assets/Vector_database")
 
     def generate_random_string(self, length: int=12):
         return ''.join(random.choices(string.ascii_lowercase + string.digits, k=length))
+    
+    def get_database_path(self, db_name: str):
+        
+            database_path = os.path.join(
+                self.database_dir, db_name
+            )
+
+            if not os.path.exists(database_path):
+                os.makedirs(database_path)
+
+            return database_path

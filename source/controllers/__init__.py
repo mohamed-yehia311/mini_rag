ab8ac3949a6c49db2a06b6941326d77efbf2b800
mohamed-yehia311 <mmoa311311@gmail.com>
2025-06-17 04:44:08 +0300

    fix: add missing imports for ChatController and BaseController in __init__.py

--- source/controllers/__init__.py
+++ source/controllers/__init__.py
@@ -1,3 +1,5 @@
 from .DataController import DataController
 from .ProjectController import ProjectController
 from .ProcessController import ProcessController
+from .ChatController import ChatController
+from .BaseController import BaseController
